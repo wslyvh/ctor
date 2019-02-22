309f71789969eb2cb4da45542c3ab9ca5020ae3a
wslyvh <wslyvh@users.noreply.github.com>
2019-02-22 16:56:58 +0100

    contract definition

--- src/components/contract/Definition.tsx
+++ src/components/contract/Definition.tsx
@@ -11,6 +11,41 @@ class ContractDefinition extends Component {
             <div className="panel">
                 <h3>{daoContract.result[0].ContractName}</h3>
                 <div>
+                    <h4>Constants</h4>
+                    <div className="alert alert-primary" role="alert">
+                        <span className="badge badge-primary">constant</span> &nbsp;
+                        <strong>rewardAccount</strong> <small>(address)</small>
+                    </div>
+                    <div className="alert alert-primary" role="alert">
+                        <span className="badge badge-primary">constant</span> &nbsp;
+                        <strong>minTokensToCreate</strong> <small>(uint256)</small>
+                    </div>
+                    <div className="alert alert-primary" role="alert">
+                        <span className="badge badge-primary">constant</span> &nbsp;
+                        <strong>proposals</strong> <small>(uint256)</small>
+                    </div>
+                    <div className="alert alert-primary" role="alert">
+                        <span className="badge badge-primary">constant</span> &nbsp;
+                        <strong>checkProposalCode</strong> <small>_proposalID (uint256), _recipient (address), _amount (uint256), _transactionData (bytes)</small>
+                    </div>
+                    
+                    <h4>Functions</h4>
+                    <div className="alert alert-success" role="alert">
+                        <span className="badge badge-success">function</span> &nbsp;
+                        <strong>transfer</strong> <small>_to, (address), _value (uint256)</small>
+                    </div>
+                    <div className="alert alert-success" role="alert">
+                        <span className="badge badge-success">function</span> &nbsp;
+                        <strong>newProposal</strong> <small>_recipient (address), _amount (uint256), _description (string), _transactionData (bytes), _debatingPeriod (uint256), _newCurator (bool)</small>
+                    </div>
+                    
+                    <h4>Events</h4>
+                    <div className="alert alert-warning" role="alert">
+                        <span className="badge badge-warning">event</span> &nbsp;
+                        <strong>AllowedRecipientChanged</strong> <small>_recipient (address), _allowed (bool)</small>
+                    </div>
+                </div>
+                <div hidden>
                     {daoContract.result[0].ABI}
                 </div>
             </div>
--- src/components/contract/Summary.tsx
+++ src/components/contract/Summary.tsx
@@ -5,7 +5,7 @@ class ContractSummary extends Component {
     public render() {
         return (
 
-            <div>
+            <div className="contract-summary">
                 <h2>The DAO</h2>
                 <h3 className="text-muted">0xBB9bc244D798123fDe783fCc1C72d3Bb8C189413 
                     <small><a href="https://etherscan.io/address/0xBB9bc244D798123fDe783fCc1C72d3Bb8C189413" className="text-secondary" target="_blank"><FaExternalLinkSquareAlt /></a></small>
