fb7714d5d5db97a07cedac858f448a03e441e3a9
wslyvh <wslyvh@users.noreply.github.com>
2019-02-27 16:54:42 +0100

    lint fix

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
