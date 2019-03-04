01e7f8d0f8732ad60aaed27dbdc77866f9cd62c4
wslyvh <wslyvh@users.noreply.github.com>
2019-03-04 16:33:06 +0100

    searchbar

---
+++ src/model/IContract.ts
@@ -0,0 +1,34 @@
+export interface IEtherscanAPIResponse {
+    readonly message: "NOTOK" | "OK";
+    readonly result: any;
+    readonly status: "1" | "0";
+}
+
+export interface IInput {
+    readonly name: string;
+    readonly type: string;
+    readonly indexed?: boolean;
+}
+
+export interface IOutput {
+    readonly name: string;
+    readonly type: string;
+}
+
+export interface ITuple extends IOutput {
+    readonly type: "tuple";
+    readonly components: ReadonlyArray<IOutput>;
+}
+
+export interface IContractMember {
+    readonly constant?: boolean;
+    readonly inputs?: ReadonlyArray<IInput>;
+    readonly name?: string;
+    readonly outputs?: ReadonlyArray<IOutput | ITuple>;
+    readonly type: string;
+    readonly payable?: boolean;
+    readonly stateMutability?: string;
+    readonly anonymous?: boolean;
+}
+
+export type ContractABI = ReadonlyArray<IContractMember>;
