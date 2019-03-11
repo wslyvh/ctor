d2c71dde6c4fd47ad70655862682b96f304a84ac
wslyvh <wslyvh@users.noreply.github.com>
2019-03-11 21:35:00 +0100

    web3Utils

---
+++ client/src/utils/Web3Utils.tsx
@@ -0,0 +1,26 @@
+import { ethers } from "ethers";
+import { BaseProvider } from "ethers/providers";
+
+class Web3Utils {
+	public static isAddress(address: string): boolean {
+		try {
+			ethers.utils.getAddress(address);
+			return true;
+		} catch (ex) {
+			console.log("Error: invalid address");
+
+			return false;
+		}
+	}
+
+	public static async isContractAddress(provider: BaseProvider, address: string): Promise<boolean> {
+		if (this.isAddress(address)) {
+			const code = await provider.getCode(address);
+			if (code !== "0x") return true;
+		}
+
+		return false;
+	}
+}
+
+export default Web3Utils;
