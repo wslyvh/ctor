94fe9ced128b093406b621a97b837127cfc83d74
wslyvh <wslyvh@users.noreply.github.com>
2019-03-14 17:46:26 +0100

    file utils

---
+++ client/src/utils/FileUtils.ts
@@ -0,0 +1,44 @@
+import * as fs from "fs";
+
+class FileUtils {
+	public static GetJsonFiles(path: string): any[] {
+		const exists = fs.existsSync(path);
+		if (!exists) {
+			throw new Error("Path doesn't exist.");
+		}
+
+		const files = Array<any>();
+		const stat = fs.lstatSync(path);
+
+		// Get files from directory
+		if (stat.isDirectory()) {
+			fs.readdirSync(path).forEach((file: string) => {
+				const filePath = path.endsWith("/") ? path + file : path + "/" + file;
+
+				if (filePath.endsWith(".json")) {
+					const file = this.getFile(filePath);
+					files.push(file);
+				}
+			});
+
+			return files;
+		}
+
+		// Get single file
+		if (path.endsWith(".json")) {
+			const file = this.getFile(path);
+			files.push(file);
+		}
+
+		return files;
+	}
+
+	public static getFile(filePath: string) {
+		const buffer = fs.readFileSync(filePath, "utf8");
+		const file = JSON.parse(buffer);
+
+		return file;
+	}
+}
+
+export default FileUtils;
