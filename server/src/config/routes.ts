5ce0fc0879bc3e190a4698e9989e2d823974da7b
wslyvh <wslyvh@users.noreply.github.com>
2019-03-28 16:40:46 +0100

    delete filecontroller

--- server/src/config/routes.ts
+++ server/src/config/routes.ts
@@ -1,20 +1,15 @@
 import { Request, Response } from "express";
 import { ContractController } from "../controllers/ContractController";
-import { FileController } from "../controllers/FileController";
 
 class Routes {
 	private baseUri = "/api";
 
-	private fileController: FileController = new FileController();
 	private contractController: ContractController = new ContractController();
 
 	public routes(app: any): void {
 		app.route(this.baseUri).get((req: Request, res: Response) => {
 			res.status(200).send("Service running..");
 		});
-
-		// Files
-		app.route(this.baseUri + "/files").get(this.fileController.getFiles);
 
 		// Contracts
 		app.route(this.baseUri + "/contracts").get(this.contractController.GetContracts);
