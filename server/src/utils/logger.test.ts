0b9ae39126a82d8224907d21a69bb74c3e67ffa8
Wesley van Heije <wslyvh@users.noreply.github.com>
2019-04-04 17:27:44 +0200

    delete casing-errors

--- server/src/utils/logger.test.ts
+++
@@ -1,9 +0,0 @@
-import logger from "./Logger";
-
-describe("Creating a Logger ", () => {
-	it("exports at instance logger", () => {
-		expect(logger).toBeDefined();
-		expect(logger.level).toBe("info");
-		expect(logger.transports.length).toBe(2);
-	});
-});
