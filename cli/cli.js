5f628da4cc4e90306677959905a68fc80dd771b1
wslyvh <wslyvh@users.noreply.github.com>
2019-04-04 12:31:47 +0200

    cli vars

--- cli/cli.js
+++ cli/cli.js
@@ -1,11 +1,16 @@
 #!/usr/bin/env node
 var yargs = require("yargs");
 var pkg = require("./package.json");
-var server = require("../server/build/Index");
 
 console.info(`${pkg.name} v${pkg.version}`);
 
-serve(5000);
+process.env.NODE_ENV = "production";
+process.env.PORT = 5500;
+process.env.contracts_build_directory = "..\\truffle\\build\\contracts\\";
+process.env.CONTRACT_SERVICE = "local";
+
+var server = require("../server/build/Index");
+serve(process.env.PORT);
 
 function serve(port) {
 	console.info(`Serving application..`);
