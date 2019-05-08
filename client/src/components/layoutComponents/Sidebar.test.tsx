c8d3869d1b59e8396b4f8bc52ac0045ea796a515
wslyvh <wslyvh@users.noreply.github.com>
2019-05-08 11:33:57 +0200

    Fix sidebar test

--- client/src/components/layoutComponents/Sidebar.test.tsx
+++ client/src/components/layoutComponents/Sidebar.test.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Sidebar from "./Sidebar";
+import { BrowserRouter, Switch } from "react-router-dom";
 
 it("renders without crashing", () => {
 	const div = document.createElement("div");
-	ReactDOM.render(<Sidebar />, div);
+	ReactDOM.render(
+		<BrowserRouter>
+			<Switch>
+				<Sidebar />
+			</Switch>
+		</BrowserRouter>, div);
 	ReactDOM.unmountComponentAtNode(div);
 });
