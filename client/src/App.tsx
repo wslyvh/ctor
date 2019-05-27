41d86215ac06f37eef03a9174035d61825ad0fe4
wslyvh <wslyvh@users.noreply.github.com>
2019-05-27 22:13:31 +0200

    remove empty pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,21 +3,19 @@ import { Redirect, Switch } from "react-router";
 import Main from "./layouts/Main";
 import Contract from "./pages/Contract";
 import Contracts from "./pages/Contracts";
-import Dashboard from "./pages/Dashboard";
-import Settings from "./pages/Settings";
 
 class App extends Component {
-	public render() {
-		return (
-			<Switch>
-				<Main exact path="/" component={Dashboard} title="Dashboard" />
-				<Main exact path="/contracts" component={Contracts} title="Contracts" />
-				<Main exact path="/contracts/:id" component={Contract} title="Contracts" />
-				<Main exact path="/settings" component={Settings} title="Settings" />
-				<Redirect to="/" />
-			</Switch>
-		);
-	}
+  public render() {
+    return (
+      <Switch>
+        {/* <Main exact path="/" component={Dashboard} title="Dashboard" /> */}
+        <Main exact path="/contracts" component={Contracts} title="Contracts" />
+        <Main exact path="/contracts/:id" component={Contract} title="Contracts" />
+        {/* <Main exact path="/settings" component={Settings} title="Settings" /> */}
+        <Redirect to="/contracts" />
+      </Switch>
+    );
+  }
 }
 
 export default App;
--- client/src/components/layoutComponents/Sidebar.tsx
+++ client/src/components/layoutComponents/Sidebar.tsx
@@ -7,55 +7,55 @@ import logo from "../../assets/logo/logo_small.png";
 const { Sider } = Layout;
 
 class Sidebar extends Component<RouteProps> {
-	public state = {
-		collapsed: true
-	};
+  public state = {
+    collapsed: true
+  };
 
-	public onCollapse = (collapsed: boolean) => {
-		this.setState({ collapsed });
-	};
+  public onCollapse = (collapsed: boolean) => {
+    this.setState({ collapsed });
+  };
 
-	public render() {
-		const { location } = this.props;
+  public render() {
+    const { location } = this.props;
 
-		const pathname = location && location.pathname ? location.pathname : "/";
-		const selected = "/" + pathname.split("/")[1];
+    const pathname = location && location.pathname ? location.pathname : "/";
+    const selected = "/" + pathname.split("/")[1];
 
-		return (
-			<>
-				<Sider collapsible collapsed={this.state.collapsed} onCollapse={this.onCollapse}>
-					<div className="logo center">
-						<Link to="/">
-							<img alt="Ctor" src={logo} height="28" />
-						</Link>
-					</div>
+    return (
+      <>
+        <Sider collapsible collapsed={this.state.collapsed} onCollapse={this.onCollapse}>
+          <div className="logo center">
+            <Link to="/">
+              <img alt="Ctor" src={logo} height="28" />
+            </Link>
+          </div>
 
-					<Menu theme="dark" selectedKeys={[selected]} mode="inline">
-						<Menu.Item key="/">
-							<Link to="/">
-								<Icon type="dashboard" />
-								<span>Dashboard</span>
-							</Link>
-						</Menu.Item>
+          <Menu theme="dark" selectedKeys={[selected]} mode="inline">
+            {/* <Menu.Item key="/">
+				<Link to="/">
+					<Icon type="dashboard" />
+					<span>Dashboard</span>
+				</Link>
+			</Menu.Item> */}
 
-						<Menu.Item key="/contracts">
-							<Link to="/contracts">
-								<Icon type="database" />
-								<span>Contracts</span>
-							</Link>
-						</Menu.Item>
+            <Menu.Item key="/contracts">
+              <Link to="/contracts">
+                <Icon type="database" />
+                <span>Contracts</span>
+              </Link>
+            </Menu.Item>
 
-						<Menu.Item key="/settings" style={{ position: "absolute", bottom: 45 }}>
-							<Link to="/settings">
-								<Icon type="setting" />
-								<span>Settings</span>
-							</Link>
-						</Menu.Item>
-					</Menu>
-				</Sider>
-			</>
-		);
-	}
+            {/* <Menu.Item key="/settings" style={{ position: "absolute", bottom: 45 }}>
+				<Link to="/settings">
+					<Icon type="setting" />
+					<span>Settings</span>
+				</Link>
+			</Menu.Item> */}
+          </Menu>
+        </Sider>
+      </>
+    );
+  }
 }
 
 export default Sidebar;
