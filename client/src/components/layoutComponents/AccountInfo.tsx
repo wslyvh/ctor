30d1515592ec3e3ea6ad038dea417b347e563231
wslyvh <wslyvh@users.noreply.github.com>
2019-05-27 19:22:42 +0200

    searchbar

--- client/src/components/layoutComponents/AccountInfo.tsx
+++ client/src/components/layoutComponents/AccountInfo.tsx
@@ -4,33 +4,33 @@ import { INetworkService } from "../../services/INetworkService";
 import { NetworkService } from "../../services/NetworkService";
 
 class AccountInfo extends Component {
-	public state = {
-		accounts: new Array<string>()
-	};
+  public state = {
+    accounts: new Array<string>()
+  };
 
-	private networkService: INetworkService = new NetworkService();
+  private networkService: INetworkService = new NetworkService();
 
-	public async componentDidMount() {
-		const accounts = await this.networkService.GetAccounts();
+  public async componentDidMount() {
+    const accounts = await this.networkService.GetAccounts();
 
-		this.setState({
-			accounts
-		});
-	}
+    this.setState({
+      accounts
+    });
+  }
 
-	public render() {
-		return (
-			<>
-				{this.state.accounts[0] && (
-					<div style={{ margin: "0 24px" }}>
-						<small style={{ margin: "0 12px" }}>{this.state.accounts[0]}</small>
-						&nbsp;
-						<Avatar shape="square" icon="user" />
-					</div>
-				)}
-			</>
-		);
-	}
+  public render() {
+    return (
+      <>
+        {this.state.accounts[0] && (
+          <div>
+            <small style={{ margin: "0 12px" }}>{this.state.accounts[0]}</small>
+            &nbsp;
+            <Avatar shape="square" icon="user" />
+          </div>
+        )}
+      </>
+    );
+  }
 }
 
 export default AccountInfo;
--- client/src/layouts/Main.tsx
+++ client/src/layouts/Main.tsx
@@ -1,52 +1,60 @@
-import { Breadcrumb, Icon, Layout } from "antd";
+import { Breadcrumb, Col, Icon, Layout, Row } from "antd";
 import React, { Component } from "react";
 import { Route, RouteProps } from "react-router-dom";
 import "../assets/styles/app.scss";
 import AccountInfo from "../components/layoutComponents/AccountInfo";
+import SearchBar from "../components/layoutComponents/SearchBar";
 import Sidebar from "../components/layoutComponents/Sidebar";
 
 const { Header, Content, Footer } = Layout;
 
 interface ILayoutProps extends RouteProps {
-	title: string;
+  title: string;
 }
 
 class Main extends Component<ILayoutProps> {
-	public render() {
-		return (
-			<>
-				<Layout style={{ minHeight: "100vh" }}>
-					<Sidebar {...this.props} />
+  public render() {
+    return (
+      <>
+        <Layout style={{ minHeight: "100vh" }}>
+          <Sidebar {...this.props} />
 
-					<Layout>
-						<Header style={{ background: "#fff", padding: 0, textAlign: "right" }}>
-							<AccountInfo />
-						</Header>
-						<Content style={{ margin: "0 16px" }}>
-							<Breadcrumb style={{ margin: "16px 0" }}>
-								<Breadcrumb.Item>{this.props.title}</Breadcrumb.Item>
-							</Breadcrumb>
-							<div style={{ padding: 24, background: "#fff" }}>
-								<Route {...this.props} />
-							</div>
-						</Content>
-						<Footer style={{ textAlign: "center" }}>
-							<small className="text-muted">
-								<a href="https://github.com/wslyvh/ctor" target="_blank" rel="noopener noreferrer" className="text-reset">
-									<Icon type="github" />
-								</a>
-								&nbsp;&nbsp;
-								<a href="https://twitter.com/wslyvh" target="_blank" rel="noopener noreferrer" className="text-reset">
-									<Icon type="twitter" />
-								</a>
-								&nbsp; @wslyvh
-							</small>
-						</Footer>
-					</Layout>
-				</Layout>
-			</>
-		);
-	}
+          <Layout>
+            <Header style={{ background: "#fff", padding: 0 }}>
+              <Row>
+                <Col span={12} style={{ textAlign: "left", padding: "0 24px" }}>
+                  <SearchBar />
+                </Col>
+                <Col span={12} style={{ textAlign: "right", padding: "0 24px" }}>
+                  <AccountInfo />
+                </Col>
+              </Row>
+            </Header>
+            <Content style={{ margin: "0 16px" }}>
+              <Breadcrumb style={{ margin: "16px 0" }}>
+                <Breadcrumb.Item>{this.props.title}</Breadcrumb.Item>
+              </Breadcrumb>
+              <div style={{ padding: 24, background: "#fff" }}>
+                <Route {...this.props} />
+              </div>
+            </Content>
+            <Footer style={{ textAlign: "center" }}>
+              <small className="text-muted">
+                <a href="https://github.com/wslyvh/ctor" target="_blank" rel="noopener noreferrer" className="text-reset">
+                  <Icon type="github" />
+                </a>
+                &nbsp;&nbsp;
+                <a href="https://twitter.com/wslyvh" target="_blank" rel="noopener noreferrer" className="text-reset">
+                  <Icon type="twitter" />
+                </a>
+                &nbsp; @wslyvh
+              </small>
+            </Footer>
+          </Layout>
+        </Layout>
+      </>
+    );
+  }
 }
 
 export default Main;
