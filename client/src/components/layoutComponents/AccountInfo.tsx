1e3d55c7cfea5220fa09196b5017d39e0452cf96
wslyvh <wslyvh@users.noreply.github.com>
2019-05-30 18:40:16 +0200

    responsive header

--- client/src/components/layoutComponents/AccountInfo.tsx
+++ client/src/components/layoutComponents/AccountInfo.tsx
@@ -6,33 +6,34 @@ import { NetworkService } from "../../services/NetworkService";
 const { Paragraph } = Typography;
 
 class AccountInfo extends Component {
-  public state = {
-    accounts: new Array<string>()
-  };
+	public state = {
+		accounts: new Array<string>(),
+		defaultAccount: ""
+	};
 
-  private networkService: INetworkService = new NetworkService();
+	private networkService: INetworkService = new NetworkService();
 
-  public async componentDidMount() {
-    const accounts = await this.networkService.GetAccounts();
+	public async componentDidMount() {
+		const accounts = await this.networkService.GetAccounts();
 
-    this.setState({
-      accounts
-    });
-  }
+		this.setState({
+			accounts,
+			defaultAccount: accounts[0]
+		});
+	}
 
-  public render() {
-    return (
-      <>
-        {this.state.accounts[0] && (
-          <Paragraph ellipsis>
-            <small style={{ margin: "0 12px" }}>{this.state.accounts[0]}</small>
-            &nbsp;
-            <Avatar shape="square" icon="user" />
-          </Paragraph>
-        )}
-      </>
-    );
-  }
+	public render() {
+		return (
+			<>
+				{this.state.defaultAccount && (
+					<Paragraph ellipsis>
+						<small className="account-address">{this.state.defaultAccount} &nbsp;</small>
+						<Avatar shape="square" icon="user" size="small" alt={this.state.defaultAccount} />
+					</Paragraph>
+				)}
+			</>
+		);
+	}
 }
 
 export default AccountInfo;
